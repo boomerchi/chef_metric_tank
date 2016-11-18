6812e7aedbef3d158c9321914815862357c26112
Dieter Plaetinck <dieter@raintank.io>
2016-11-18 10:04:38 +0200

    bump version to sync with MT version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@raintank.io'
 license          'Apache 2.0'
 description      'Installs/Configures metrictank'
 long_description 'Installs/Configures metrictank'
-version          '0.2.4'
+version          '0.6.0'
 
 depends 'chef_base', '~> 0.1.0'
 depends 'logrotate', '~> 1.9.1'
